abe057a8f5e33c553fd58a525a17c29c781a1a7c
shubham <shubham.basu991@gmail.com>
2017-09-26 01:45:19 -0400

    View Change
    Added Constants and made changes in view to display multiple object at
    same time

---
+++ GameMaker/src/game/modal/Constants.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package game.modal;
+
+/**
+ * @author shubh
+ *
+ */
+public class Constants {
+    
+	//speed of the ball
+	public static int MOVEMENT_X = 5;
+	public static int MOVEMENT_Y = 5;
+	
+	
+	
+	//size for paddle sprite
+	public static int PADDLE_HEIGHT = 50;
+	public static int PADDLE_WIDTH = 200;
+	
+	//size of frame
+	public static int FRAME_WIDTH=800;
+	public static int FRAME_HEIGHT=800;
+	
+	//default size of panel
+	public static int PANEL_WIDTH = 782;
+	public static int PANEL_HEIGTH = 753;
+	
+	public static int BALL_HEIGHT = 40;
+	public static int BALL_WIDTH = 40;
+	
+	
+	
+}
--- GameMaker/src/game/view/GameView.java
+++ GameMaker/src/game/view/GameView.java
@@ -9,27 +9,120 @@ import java.awt.Image;
 import java.awt.Panel;
 
 /**
- * @author shubham
- *it will be the view that will capture movement and request controller for data to display on screen
+ * @author shubham it will be the view that will capture movement and request
+ *         controller for data to display on screen
  */
 @SuppressWarnings("serial")
 public class GameView extends Panel {
-    
-	//the view need to know where to draw
-	private int location_X;
-	private int location_Y;
-	//the view need to know what to draw
-	private Image spriteImage;
-	//the game need to the height and width of the game object it need to paint
-	private int height;
-	private int width;
 
-	//the actual method that will paint the game objects on the screen
+	// the view need to know where to draw
+	private int autoLocation_X;
+	private int autoLocation_Y;
+	// the view need to know what to draw
+	private Image autoSpriteImage;
+	// the game need to the height and width of the game object it need to paint
+	private int autoHeight;
+	private int autoWidth;
+
+	// the view need to know where to draw
+	private int commandLocation_X;
+	private int commandLocation_Y;
+	// the view need to know what to draw
+	private Image commandSpriteImage;
+	// the game need to the height and width of the game object it need to paint
+	private int commandHeight;
+	private int commandWidth;
+
+	// the actual method that will paint the game objects on the screen
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
+		
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawImage(spriteImage, location_X,location_Y ,width,height,this);
+		g2d.drawImage(commandSpriteImage, commandLocation_X, commandLocation_Y, commandWidth, commandHeight, this);
+		g2d.drawImage(autoSpriteImage, autoLocation_X, autoLocation_Y, autoWidth, autoHeight, this);
+
 	}
+
+	public int getAutoLocation_X() {
+		return autoLocation_X;
+	}
+
+	public void setAutoLocation_X(int autoLocation_X) {
+		this.autoLocation_X = autoLocation_X;
+	}
+
+	public int getAutoLocation_Y() {
+		return autoLocation_Y;
+	}
+
+	public void setAutoLocation_Y(int autoLocation_Y) {
+		this.autoLocation_Y = autoLocation_Y;
+	}
+
+	public Image getAutoSpriteImage() {
+		return autoSpriteImage;
+	}
+
+	public void setAutoSpriteImage(Image autoSpriteImage) {
+		this.autoSpriteImage = autoSpriteImage;
+	}
+
+	public int getAutoHeight() {
+		return autoHeight;
+	}
+
+	public void setAutoHeight(int autoHeight) {
+		this.autoHeight = autoHeight;
+	}
+
+	public int getAutoWidth() {
+		return autoWidth;
+	}
+
+	public void setAutoWidth(int autoWidth) {
+		this.autoWidth = autoWidth;
+	}
+
+	public int getCommandLocation_X() {
+		return commandLocation_X;
+	}
+
+	public void setCommandLocation_X(int commandLocation_X) {
+		this.commandLocation_X = commandLocation_X;
+	}
+
+	public int getCommandLocation_Y() {
+		return commandLocation_Y;
+	}
+
+	public void setCommandLocation_Y(int commandLocation_Y) {
+		this.commandLocation_Y = commandLocation_Y;
+	}
+
+	public Image getCommandSpriteImage() {
+		return commandSpriteImage;
+	}
+
+	public void setCommandSpriteImage(Image commandSpriteImage) {
+		this.commandSpriteImage = commandSpriteImage;
+	}
+
+	public int getCommandHeight() {
+		return commandHeight;
+	}
+
+	public void setCommandHeight(int commandHeight) {
+		this.commandHeight = commandHeight;
+	}
+
+	public int getCommandWidth() {
+		return commandWidth;
+	}
+
+	public void setCommandWidth(int commandWidth) {
+		this.commandWidth = commandWidth;
+	}
+
 }
